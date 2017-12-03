fbddc9c771622b5a2d33cedbbb57120a44cb19f2
Nate Chrisman <nachrism@asu.edu>
2017-12-03 01:23:35 +0000

    Admin Team Forms Refactor

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -5,168 +5,74 @@ $(document).ready(function(){
     var count = $(this).val();
     for (var i = 0; i < count; i++) {
         $('#memberInputs').append(
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="firstName">First Name: </label>' +
-                    '<input class="form-control" type="text" name="team[members][firstName][]" id="firstName" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="lastName">Last Name: </label>' +
-                    '<input class="form-control" type="text" name="team[members][lastName][]" id="lastName" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="handle">Player Handle: </label>' +
-                    '<input class="form-control" type="text" name="team[members][handle][]" id="handle" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="role">Role: </label>' +
-                    '<input class="form-control" type="text" name="team[members][role][]" id="role" />' +
-                '</div>' +
-            '</div>' + 
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="dob">Date of Birth: </label>' +
-                    '<input class="form-control" type="text" name="team[members][dob][]" id="dob" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="homeCountry">Home Country: </label>' +
-                    '<input class="form-control" name="team[members][homeCountry][]" id="homeCountry" />' +
-                '</div>' +
-            '</div>' + 
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="image">Image URL: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][image][]" id="image" value="https://i.imgur.com/amUEiX5.png" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2"> ' +
-                    '<label for="bio">Bio: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<textarea class="form-control" name="team[members][bio][]" id="bio" rows="5">unknown</textarea>' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="instagram">Instagram: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][instagram][]" id="instagram" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="facebook">Facebook: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][facebook][]" id="facebook" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="twitch">Twitch: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][twitch][]" id="twitch" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="twitter">Twitter: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][twitter][]" id="twitter" />' +
-                '</div>' +
-            '</div>'
+            '<div class="content-form-col">' +
+                '<label for="firstName">First Name:</label>' +
+                '<input class="form-control" type="text" name="team[members][firstName][]" id="firstName" />' +
+        		'<label for="lastName">Last Name: </label>' +
+        		'<input class="form-control" type="text" name="team[members][lastName][]" id="lastName" />' +
+        		'<label for="handle">Player Handle: </label>' +
+        		'<input class="form-control" type="text" name="team[members][handle][]" id="handle"  />' +
+        		'<label for="role">Role: </label>' +
+        		'<input class="form-control" type="text" name="team[members][role][]" id="role" />' +
+        	'</div>' +
+            '<div class="content-form-col">' +
+				'<label for="dob">Date of Birth: </label>' +
+				'<input class="form-control" type="text" name="team[members][dob][]" id="dob" />' +
+				'<label for="homeCountry">Home Country: </label>' +
+				'<input class="form-control" name="team[members][homeCountry][]" id="homeCountry" />' +
+				'<label for="image">Image URL: </label>' +
+				'<input class="form-control" type="text" name="team[members][image][]" id="image" />' +
+				'<label for="bio">Bio: </label>' +
+				'<textarea class="form-control" name="team[members][bio][]" id="bio" rows="5" required>unknown</textarea>' +
+			'</div>' +
+            '<div class="content-form-col">' +
+				'<label for="instagram">Instagram: </label>' +
+				'<input class="form-control" type="text" name="team[members][instagram][]" id="instagram" />' +
+				'<label for="facebook">Facebook: </label>' +
+				'<input class="form-control" type="text" name="team[members][facebook][]" id="facebook" />' +
+				'<label for="twitch">Twitch: </label>' +
+				'<input class="form-control" type="text" name="team[members][twitch][]" id="twitch" />' +
+				'<label for="twitter">Twitter: </label>' +
+				'<input class="form-control" type="text" name="team[members][twitter][]" id="twitter" />' +
+			'</div>' +
+			'<hr>'
         );
     }
     });
     
     $("#addTeamMember").click(function () {
         $('#teamMembersEdit').append(
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="firstName">First Name: </label>' +
-                    '<input class="form-control" type="text" name="team[members][firstName][]" id="firstName" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="lastName">Last Name: </label>' +
-                    '<input class="form-control" type="text" name="team[members][lastName][]" id="lastName" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="handle">Player Handle: </label>' +
-                    '<input class="form-control" type="text" name="team[members][handle][]" id="handle" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="role">Role: </label>' +
-                    '<input class="form-control" type="text" name="team[members][role][]" id="role" />' +
-                '</div>' +
-            '</div>' + 
-            '<div class="form-group row">' + 
-                '<div class="col-xs-6 col-md-6">' + 
-                    '<label for="dob">Date of Birth: </label>' +
-                    '<input class="form-control" type="text" name="team[members][dob][]" id="dob" />' +
-                '</div>' +
-                '<div class="col-xs-6 col-md-6">' +
-                    '<label for="homeCountry">Home Country: </label>' +
-                    '<input class="form-control" name="team[members][homeCountry][]" id="homeCountry" />' +
-                '</div>' +
-            '</div>' + 
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="image">Image URL: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][image][]" id="image" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2"> ' +
-                    '<label for="bio">Bio: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<textarea class="form-control" name="team[members][bio][]" id="bio" rows="5">unknown</textarea>' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="instagram">Instagram: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][instagram][]" id="instagram" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="facebook">Facebook: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][facebook][]" id="facebook" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="twitch">Twitch: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][twitch][]" id="twitch" />' +
-                '</div>' +
-            '</div>' +
-            '<div class="form-group row">' +
-                '<div class="col-xs-2 col-md-2">' +
-                    '<label for="twitter">Twitter: </label>' +
-                '</div>' +
-                '<div class="col-xs-10 col-md-10">' +
-                    '<input class="form-control" type="text" name="team[members][twitter][]" id="twitter" />' +
-                '</div>' +
-            '</div>'
+            '<div class="content-form-col">' +
+                '<label for="firstName">First Name:</label>' +
+                '<input class="form-control" type="text" name="team[members][firstName][]" id="firstName" />' +
+        		'<label for="lastName">Last Name: </label>' +
+        		'<input class="form-control" type="text" name="team[members][lastName][]" id="lastName" />' +
+        		'<label for="handle">Player Handle: </label>' +
+        		'<input class="form-control" type="text" name="team[members][handle][]" id="handle"  />' +
+        		'<label for="role">Role: </label>' +
+        		'<input class="form-control" type="text" name="team[members][role][]" id="role" />' +
+        	'</div>' +
+            '<div class="content-form-col">' +
+				'<label for="dob">Date of Birth: </label>' +
+				'<input class="form-control" type="text" name="team[members][dob][]" id="dob" />' +
+				'<label for="homeCountry">Home Country: </label>' +
+				'<input class="form-control" name="team[members][homeCountry][]" id="homeCountry" />' +
+				'<label for="image">Image URL: </label>' +
+				'<input class="form-control" type="text" name="team[members][image][]" id="image" />' +
+				'<label for="bio">Bio: </label>' +
+				'<textarea class="form-control" name="team[members][bio][]" id="bio" rows="5" required>unknown</textarea>' +
+			'</div>' +
+            '<div class="content-form-col">' +
+				'<label for="instagram">Instagram: </label>' +
+				'<input class="form-control" type="text" name="team[members][instagram][]" id="instagram" />' +
+				'<label for="facebook">Facebook: </label>' +
+				'<input class="form-control" type="text" name="team[members][facebook][]" id="facebook" />' +
+				'<label for="twitch">Twitch: </label>' +
+				'<input class="form-control" type="text" name="team[members][twitch][]" id="twitch" />' +
+				'<label for="twitter">Twitter: </label>' +
+				'<input class="form-control" type="text" name="team[members][twitter][]" id="twitter" />' +
+			'</div>' +
+			'<hr>'
         );
     });
     
