68ecf94c773fed034aa78b887866a6fe0f35117a
Nate Chrisman <nachrism@asu.edu>
2018-01-13 23:44:51 +0000

    Added Game Franchise Functionality

--- models/game.js
+++ models/game.js
@@ -1,32 +1,20 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-var genres = ['FPS', 'MOBA', 'TPS', 'Fighting'];
-var platforms = ['PC', 'Playstation', 'Xbox', 'Switch',
-					'Wii U', 'GameCube'];
+var genres = ["FPS", "MOBA", "TPS", "Fighting"],
+	platforms = ["PC", "Playstation", "Xbox", "Switch", "Wii U", "GameCube"];
 
 var gameSchema = new mongoose.Schema({
 	title: String,
-	abbreviation: {type: String, default: 'None'},
-	platform: {
-		type: String,
-		enum: platforms,
-		default: null
-		},
+	abbreviation: {type: String, default: "None"},
+	franchise: String,
+	platform: {type: String, enum: platforms, default: null},
+	genre: {type: String, enum: genres, default: "none"},
 	logojpg: String,
 	logopng: String,
-	genre: {
-		type: String,
-		enum: genres,
-		default: 'none'
-	}
+	active: {type: Boolean, default: false}
 });
 
-gameSchema.virtual('genres').get(function () {
-    return genres;
-});
+gameSchema.virtual("genres").get(function(){ return genres; });
+gameSchema.virtual("platforms").get(function(){ return platforms; });
 
-gameSchema.virtual('platforms').get(function () {
-    return platforms;
-});
-
-module.exports = mongoose.model('Game', gameSchema);
+module.exports = mongoose.model("Game", gameSchema);
